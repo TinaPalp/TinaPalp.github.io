596714dc625fd92460c62cd847336906490726b2
TinaPalp <valentina.palpanes@gmail.com>
2021-07-14 20:28:51 +0200

    Create app.js

---
+++ LandingPage/js/app.js
@@ -0,0 +1,116 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+ * 
+*/
+
+const navBarList = document.getElementById("navbar__list");
+const section = document.getElementsByTagName("section");
+const headlines = document.getElementsByTagName("h2");
+
+
+
+// creating nav-bar 
+for(var i=0; i<section.length; i++) {
+    let liNav = document.createElement('li');
+
+    if(i==0){
+        liNav.classList.add('activeNavItem');
+    }
+
+    let aNav = document.createElement('a');
+    aNav.setAttribute('href','#'+section[i].id );
+    let headlineNav = document.createTextNode(headlines[i].innerText);
+    aNav.classList.add('menu__link');
+    aNav.appendChild(headlineNav);
+    navBarList.appendChild(liNav);
+    liNav.appendChild(aNav);
+
+
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        //rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= window.innerHeight &&
+        rect.right <= window.innerWidth 
+    );
+}
+
+function whichSection() {
+    let sectionViewport = 10;
+    for(var i=0; i<section.length; i++) {
+        if(isInViewport(section[i])) {
+            sectionViewport = i;
+        } 
+    }
+    return(sectionViewport);
+}
+
+function viewportSection() {
+   highlightSectionAndNav()
+}
+
+function highlightSectionAndNav(){
+    sectionInView = whichSection();
+    const activeSection = document.getElementsByClassName('your-active-class');
+    const activeNav = document.getElementsByClassName('activeNavItem');
+    const activeNavList = document.getElementsByTagName('li');
+
+
+
+    if (sectionInView !=10) {
+        section[sectionInView].classList.add('your-active-class');
+        activeNavList[sectionInView].classList.add('activeNavItem');
+        for(var i=0; i<activeSection.length; i++) {
+            if (activeSection[i].id != section[sectionInView].id) {
+                activeSection[i].classList.remove('your-active-class');
+                activeNav[i].classList.remove('activeNavItem');
+
+            }
+        } 
+    }
+    else{
+        section[0].classList.remove('your-active-class');
+        activeNavList[0].classList.remove('activeNavItem');
+
+    }
+
+
+}
+
+// Add class 'active' to section when near top of viewport
+
+document.addEventListener('scroll', viewportSection);
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
