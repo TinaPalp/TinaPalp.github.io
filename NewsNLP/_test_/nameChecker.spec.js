a1db3eee050692ecffd2028a932b9616cfa6be0d
TinaPalp <valentina.palpanes@gmail.com>
2021-08-22 20:13:16 +0200

    Create nameChecker.spec.js

---
+++ NewsNLP/_test_/nameChecker.spec.js
@@ -0,0 +1,6 @@
+// Import the js file to test
+import { checkForName } from "../src/client/js/nameChecker";
+describe("Testing the URL validity", () => {
+    test("Testing the checkForName() function", () => {
+           expect(checkForName).toBeDefined();
+})});
