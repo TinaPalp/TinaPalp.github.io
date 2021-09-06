3509dd69a1e6391e112a92176b872d36837b0ce1
TinaPalp <valentina.palpanes@gmail.com>
2021-09-06 09:26:24 +0200

    Create getCountdown.spec.js

---
+++ Capstone/_test_/getCountdown.spec.js
@@ -0,0 +1,6 @@
+// Import the js file to test
+import { getCountdown } from "../src/client/js/getCountdown";
+describe("Testing the submit functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+           expect(getCountdown).toBeDefined();
+})});
