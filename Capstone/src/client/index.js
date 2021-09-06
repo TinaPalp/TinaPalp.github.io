f134552aabb185c738996302ad4f3951ce283037
TinaPalp <valentina.palpanes@gmail.com>
2021-09-06 09:27:04 +0200

    Create index.js

---
+++ Capstone/src/client/index.js
@@ -0,0 +1,10 @@
+import { generateOutput } from './js/formHandler'
+import { getCountdown } from './js/getCountdown'
+import  { updateUI } from './js/formHandler'
+
+
+import './styles/general_style.scss'
+
+
+
+
